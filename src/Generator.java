8b2a66ad8ad9c7d2f20c0ccb6083c1a0f2e60430
ShadowproofGamer <kubac400@gmail.com>
2022-04-29 07:54:13 +0200

    final

--- src/Generator.java
+++ src/Generator.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 
+import static java.lang.Math.abs;
+
 public class Generator {
     public static int[] generate(int number, int pages){
         Random rand = new Random();
@@ -13,4 +15,54 @@ public class Generator {
         }
         return result;
     }
+    public static int[] generateIncLocality(int number, int pages){
+        Random rand = new Random();
+        int temp = 0;
+        int[] result = new int[number];
+        for (int i = 0; i < number; i++) {
+            int op = rand.nextInt(10);
+            int r=temp;
+            if (op<=0)r=abs(temp-1)%pages;
+            else if (op<=1)r=abs(temp+1)%pages;
+            else if (op>=8)r=rand.nextInt(pages);
+            result[i]=r;
+            temp=r;
+        }
+        return result;
+    }
+    public static int[] generateIncLocality2(int number, int pages){
+        Random rand = new Random();
+        int temp = 0;
+        int[] result = new int[number];
+        for (int i = 0; i < number; i++) {
+            int op = rand.nextInt(10);
+            int r=temp;
+            if (op<=3)r=abs(temp-1)%pages;
+            else if (op<=7)r=abs(temp+1)%pages;
+            else if (op>=9)r=rand.nextInt(pages);
+            result[i]=r;
+            temp=r;
+        }
+        return result;
+    }
+    public static int[] generateIncLocalityOptions(int number, int pages, int lower, int higher, int same, int random){
+        System.out.println("jeden mniejsze: "+ lower+"\tjeden większe: "+ higher+"\ttakie same:"+ same+"\tlosowe:"+ random);
+        Random rand = new Random();
+        int temp = 0;
+        int[] result = new int[number];
+        for (int i = 0; i < number; i++) {
+            int op = rand.nextInt(lower+higher+same+random);
+            int r=temp;
+            if (op<=lower){
+                r=temp-1;
+                if (r<0)r+=pages;
+            }
+            else if (op<=(lower+higher))r=abs(temp+1)%pages;
+            else if (op<=(lower+higher+random))r=rand.nextInt(pages);
+            result[i]=r;
+            temp=r;
+        }
+        return result;
+    }
+
 }
